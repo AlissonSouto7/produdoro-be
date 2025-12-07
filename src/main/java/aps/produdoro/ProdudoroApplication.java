880be1aba0f314a5340ee5af79e1c577aceafe62
AlissonSouto7 <alissonprogp1@gmail.com>
2025-12-07 09:41:41 -0300

    Initial commit: setting up Spring Boot project structure

---
+++ src/main/java/aps/produdoro/ProdudoroApplication.java
@@ -0,0 +1,12 @@
+package aps.produdoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdudoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdudoroApplication.class, args);
+	}
+}
